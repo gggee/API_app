7b40102fc08ebfdd45fd67b28d0625f3c358f57b
ggg <gg.gsw4@gmail.com>
2023-08-23 00:27:16 +0600

    third commit

--- src/app/api-serv.service.ts
+++ src/app/api-serv.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ApiServService {
 
-  constructor() { }
+export class ApiServService {
+  constructor(private http: HttpClient) { }
+
+  userLogin!: string;
+  userPassw!: string;
 }
